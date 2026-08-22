5f92d3b94103f1d996807a004755c348d1210207
JosephPHarrison <joe2chris@hotmail.com>
2026-08-22 18:25:21 +0000

    Added button to movie details page to add a movie to list of favourites

--- src/app/movie-details/movie-details.page.ts
+++ src/app/movie-details/movie-details.page.ts
@@ -1,18 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar, IonCard, IonCardHeader, IonCardTitle, IonCardContent, } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonCard, IonCardHeader, IonCardTitle, IonCardContent, IonButton } from '@ionic/angular/standalone';
 import { ActivatedRoute, Router, } from '@angular/router';
 import { MovieService } from '../services/movie.service';
 import { Movie } from '../models/movie.model';
 import { CastMember, CrewMember } from '../models/credits.model';
+import { FavouritesService } from '../services/favourites.service';
 
 @Component({
   selector: 'app-movie-details',
   templateUrl: './movie-details.page.html',
   styleUrls: ['./movie-details.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonCard, IonCardHeader, IonCardTitle, IonCardContent]
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonCard, IonCardHeader, IonCardTitle, IonCardContent, IonButton]
 })
 export class MovieDetailsPage implements OnInit {
 
@@ -25,8 +26,11 @@ export class MovieDetailsPage implements OnInit {
   // The movie's crew, starts as empty, then taken from teh credits
   crew: CrewMember[] = [];
 
-  // ActivatedRoute reads the movie ID fro mthe URL, MovieServices fetches the data
-  constructor(private route: ActivatedRoute, private movieService: MovieService, private router: Router) {
+  // Tracks whether the current movie is in the favourites list, controlling the button label
+  isFavourited: boolean = false;
+
+  // ActivatedRoute reads the movie ID fro mthe URL, MovieServices fetches the data, Router navigates to other pages, FavouriteServe manages the favourites list
+  constructor(private route: ActivatedRoute, private movieService: MovieService, private router: Router, private favouritesService: FavouritesService) {
   }
 
   ngOnInit() {
@@ -40,6 +44,7 @@ export class MovieDetailsPage implements OnInit {
         this.movie = response;
         this.cast = response.credits.cast;
         this.crew = response.credits.crew;
+        this.checkFavourites();
       });
     }
   }
@@ -49,4 +54,18 @@ export class MovieDetailsPage implements OnInit {
     this.router.navigate(['/person-details', id]);
   }
 
+  // Checks thwther the current movie is already in the favourites list, updates the button state
+  async checkFavourites() {
+    this.isFavourited = await this.favouritesService.isFavourite(this.movie.id);
+  }
+
+  // Toggles the movie in or out of hte favourites lsit depending on its current state
+  async toggleFavourite() {
+    if (this.isFavourited) {
+      await this.favouritesService.removeFavourite(this.movie.id);
+    } else {
+      await this.favouritesService.addFavourite(this.movie);
+    }
+    this.checkFavourites();
+  }
 }
